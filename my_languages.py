c70f4164df44af3dd3ddd55dbf8cf2fd119165a8
KG <kgitts98@gmail.com>
2023-01-16 08:30:08 -0800

    .

--- my_languages.py
+++ my_languages.py
@@ -4,7 +4,15 @@ def my_languages(results):
     print(k, v)
   """
 
-  return sorted((l for l, r in results.items() if r >= 60), reverse = True, key = results.get)
+  # return sorted((l for l, r in results.items() if r >= 60), reverse = True, key = results.get)
+
+  array_list = []
+  sorted_results = (sorted(results.items(), key = lambda k: k[1], reverse = True))
+  print("sorted_results =", sorted_results)
+  for i in sorted_results:
+    if i[1] >= 60:
+      array_list.append(i[0])
+  return array_list
 
 
 print(my_languages({"Java": 10, "Ruby": 80, "Python": 65}))
