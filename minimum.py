2be145ef5f9d1a9104ad87c077ce3fad3e52da88
KG <kgitts98@gmail.com>
2023-01-19 11:58:22 -0800

    .

--- minimum.py
+++ minimum.py
@@ -1,4 +1,11 @@
 def minimum(a, x):
-  return min(a % x, -a % x)
+  # return min(a % x, -a % x)
+  # return abs(a-round(a/x)*x)
+  near = x
+  while near < a:
+    near += x
+  plus = near - a
+  minus = a-(near-x)
+  return plus if plus < minus else minus 
 
 print(minimum(10, 6))
