4018ff4240197367d90394223a1db87f51f68ef8
KG <kgitts98@gmail.com>
2023-01-19 14:55:00 -0800

    .

--- count_correct_characters.py
+++ count_correct_characters.py
@@ -6,7 +6,14 @@ def count_correct_characters(correct, guess):
     print(i if i in char_list)
   """
 
+  """
   if len(correct) != len(guess): raise Exception('Error')
   return sum(1 for i, j in zip(correct, guess) if i==j)
+  """
+
+  if len(correct) == len(guess):
+    return sum(a == b for a, b in zip(correct, guess))
+  else:
+    raise ValueError("The given strings are of different lengths.")
 
 print(count_correct_characters("dog", "god"))
