54a2bfa6fd5e29b592a2aef639651c5dfde1b05c
KG <kgitts98@gmail.com>
2023-01-19 15:14:18 -0800

    .

--- what_note.py
+++ what_note.py
@@ -1,5 +1,9 @@
 def what_note(string, fret):
+  """
   notes = "A, A#, B, C, D, D#, E, F, F#, G, G#".split(', ')
   return notes[(notes.index(string.upper()) + fret)%len(notes)]
+  """
+  
+  E = ['E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#']
 
 print(what_note("e", 0))
