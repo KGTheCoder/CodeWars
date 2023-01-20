df495daf2496d33c93e66339092500814c25b85e
KG <kgitts98@gmail.com>
2023-01-20 07:24:33 -0800

    .

--- shades_of_grey.py
+++ shades_of_grey.py
@@ -1,4 +1,4 @@
 def shades_of_grey(n):
-  return ['#{0:02x}{0:02x}{0:02x}'.format(i+1) for i in range(min(254, n))]
+  # return ['#{0:02x}{0:02x}{0:02x}'.format(i+1) for i in range(min(254, n))]
 
-print(shades_of_grey(1))
+print(shades_of_grey(2))
