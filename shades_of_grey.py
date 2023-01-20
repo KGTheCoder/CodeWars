483ff67968f5097dc3719a5b5c62594aa74d09f7
KG <kgitts98@gmail.com>
2023-01-20 07:36:30 -0800

    .

--- shades_of_grey.py
+++ shades_of_grey.py
@@ -1,4 +1,23 @@
 def shades_of_grey(n):
   # return ['#{0:02x}{0:02x}{0:02x}'.format(i+1) for i in range(min(254, n))]
 
-print(shades_of_grey(2))
+  """
+  my_list = []
+  if n <= 0:
+    return []
+  elif n > 254:
+    n = 254
+
+  for i in range(1, n+1):
+    h_num = hex(i)[2:]
+    if len(h_num) == 1:
+      h_num = '0' + h_num
+    h_num = '#' + h_num * 3
+    my_list.append(h_num)
+
+  return my_list
+  """
+
+  
+
+print(shades_of_grey(200))
