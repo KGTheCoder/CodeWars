18c52fc8b9c9ec2da4c4a7fd255278caf951caae
KG <kgitts98@gmail.com>
2023-01-24 11:54:30 -0800

    .

--- last.py
+++ last.py
@@ -4,6 +4,16 @@ def last(s):
   return sorted(res, key=lambda x: int(x[-1]))
   """
 
-  return sorted(s.split(), key=lambda x: x[-1])
+  # return sorted(s.split(), key=lambda x: x[-1])
+
+  a = s.split()
+  last_char = []
+  i = 0
+  for word in a:
+    last_char.append((word[-1], i, word))
+    i = i + 1
+  last_char = sorted(last_char)
+  output = [word for char, i, word in last_char]
+  return output
 
 print(last("man i need a taxi up to ubud"))
