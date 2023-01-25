f9b0ca6c1f5bcc6de623ee78bc08b77a19096267
KG <kgitts98@gmail.com>
2023-01-25 12:11:11 -0800

    .

--- reverse_number.py
+++ reverse_number.py
@@ -4,7 +4,18 @@ def reverse_number(n):
   return str(n[::-1])
   """
 
+  """
   if n < 0: return -reverse_number(-n)
   return int(str(n)[::-1])
+  """
 
-print(reverse_number(123))
+  """
+  if n >= 0:
+    return int(str(n)[::-1])
+  else:
+    return -int(str(n)[:0:-1])
+  """
+
+  
+
+print(reverse_number(-123))
