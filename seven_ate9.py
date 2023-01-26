6d64a966279ee580cd77b75d84110541c9c5028e
KG <kgitts98@gmail.com>
2023-01-26 11:27:23 -0800

    .

--- seven_ate9.py
+++ seven_ate9.py
@@ -1,7 +1,13 @@
 def seven_ate9(s):
+  """
   while s.find('797') != -1:
-    s = s.replace('797', 77)
+    s = s.replace('797', '77')
+  return s
+  """
+
+  while '797' in s:
+    s = s.replace('797', '77')
   return s
 
 
-print(seven_ate9("79712312"))
+print(seven_ate9('79712312'))
