63316ae926409c75ca5f473b221ba49a7afed087
KG <kgitts98@gmail.com>
2023-01-26 11:35:24 -0800

    .

--- seven_ate9.py
+++ seven_ate9.py
@@ -11,7 +11,20 @@
   return s
 """
 
-seven_ate9 = lambda s:''.join(x for i, x in enumerate(s) if s[i-1:i+2] != '797')
+# seven_ate9 = lambda s:''.join(x for i, x in enumerate(s) if s[i-1:i+2] != '797')
+
+def seven_ate9(s):
+  i = 0
+  n = len(s)
+  new = ''
+  while i < n:
+    if s[i:i + 3] == '797':
+      new += '7'
+      i += 2
+    else:
+      new += s[i]
+      i += 1
+  return new
 
 
 print(seven_ate9('79712312'))
