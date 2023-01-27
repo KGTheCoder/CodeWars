7aefbdd0153bfb0e8a96dfaff62c120f36e6ce64
KG <kgitts98@gmail.com>
2023-01-27 07:56:38 -0800

    .

--- square_digits.py
+++ square_digits.py
@@ -4,10 +4,19 @@ def square_digits(num):
     num += n**2
   """
 
+  """
   res = ""
   for n in str(num):
     res += str(int(n)**2)
   return int(res)
+  """
 
+  # return int(''.join(str(int(n)**2) for n in str(num)))
+
+  num = str(num)
+  ans = ''
+  for n in num:
+    ans += str(int(n)**2)
+  return ans
 
 print(square_digits(9119))
