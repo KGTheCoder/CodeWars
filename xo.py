1b674b05ce1f532f1e2591a06f1c4b9ca1058900
KG <kgitts98@gmail.com>
2023-01-27 12:49:48 -0800

    .

--- xo.py
+++ xo.py
@@ -1,25 +1,32 @@
 def xo(s):
   """
-  count = 0
-  for c in s:
-    if not "x" or "o":
-      return True
-    elif "x" in s or "o" in s:
-      count += 1
-  if count % 2 == 0:
-    print(count)
-    return True
-  else:
-    print(count)
-    return False
+  x_count = 0
+  o_count = 0
+  for char in s:
+    if 'x' or 'o' not in s:
+      return False
+    elif 'x' in s:
+      x_count += 1
+    elif 'o' in s:
+      o_count += 1
+  return x_count == o_count
   """
-  
+
   """
   s = s.lower()
   return s.count('x') == s.count('o')
   """
 
-  return s.lower().count('x') == s.lower().count('o')
+  exes = 0
+  ohs = 0
+
+  for c in s.lower():
+    if c == 'x':
+      exes += 1
+    elif c == 'o':
+      ohs += 1
+  
+  return exes == ohs
 
 
-print(xo("xOxo"))
+print(xo('xo'))
