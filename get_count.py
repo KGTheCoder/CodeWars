b3810e53b6e87123dba5682e6a48186e4be22b86
KG <kgitts98@gmail.com>
2023-01-27 07:47:17 -0800

    .

--- get_count.py
+++ get_count.py
@@ -1,10 +1,16 @@
 def get_count(sentence):
+  """
   vowels = "aeiou"
   count = 0
   for v in sentence:
     if v in vowels:
       count += 1
   return count
+  """
+
+ # return sum(1 for v in sentence if v in "aeiouAEIOU")
+
+  return sum(v in "aeiou" for v in sentence)
 
 
 print(get_count("aeiou"))
