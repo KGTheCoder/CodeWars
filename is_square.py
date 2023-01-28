bae6d44c93d8a5081d282433fa3c8a4440dfddcd
KG <kgitts98@gmail.com>
2023-01-28 08:28:52 -0800

    .

--- is_square.py
+++ is_square.py
@@ -1,17 +1,5 @@
-from math import isqrt
+def is_square(n):
+  pass
 
-def is_square(arr):
-  """
-  for i in arr:
-    return i == math.isqrt(i) ** 2
-  """
 
-  """
-  if arr:
-    return all((a ** 0.5).is_integer() for a in arr)
-  """
-
-  if arr:
-    return all(isqrt(x)**2 == x for x in arr)
-
-print(is_square([1, 4, 9, 16, 25, 36]))
+print(is_square(-1))
