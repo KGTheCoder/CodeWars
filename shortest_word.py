9992b66d36691ad484e4e975102a0d61939b8806
KG <kgitts98@gmail.com>
2023-01-28 09:35:37 -0800

    .

--- shortest_word.py
+++ shortest_word.py
@@ -1,6 +1,17 @@
 def find_short(s):
+  '''
   l = s.split()
   return len(min(l, key=len))
+  '''
+
+  # return min(len(x) for x in s.split())
+
+  sList = s.split()
+  shortestLen = len(sList[0])
+  for item in sList:
+    if len(item) < shortestLen:
+      shortestLen = len(item)
+  return shortestLen
 
 
 print(find_short('turns out random test cases are easier than writing out basic ones'))
