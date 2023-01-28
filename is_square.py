fdd706e054ed59524f34432cf9bfea0d826f1a17
KG <kgitts98@gmail.com>
2023-01-28 08:38:13 -0800

    .

--- is_square.py
+++ is_square.py
@@ -1,5 +1,15 @@
+import math
+
 def is_square(n):
-  pass
+  """
+  root = math.sqrt(n)
+  if (root + 0.5) ** 2 == n:
+    return True
+  else:
+    return False
+  """
+
+  return n > -1 and math.sqrt(n) % 1 == 0
 
 
 print(is_square(-1))
