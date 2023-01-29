98f64d9e5f123df9037d23669e54993bf6b9a6eb
KG <kgitts98@gmail.com>
2023-01-29 14:44:37 -0800

    .

--- divisible_by_three.py
+++ divisible_by_three.py
@@ -1,10 +1,16 @@
-def divisible_by_three(n):
+def divisible_by_three(st):
+  '''
   n2 = 0
   for i in n:
     n2 += int(i)
   if n2 % 3 == 0:
     return True
   return False
+  '''
+
+  while len(st) != 1:
+    st = str(sum(int(n) for n in st))
+  return int(st) in [0, 3, 6, 9]
 
 
 print(divisible_by_three('123'))
