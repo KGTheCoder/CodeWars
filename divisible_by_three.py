20305a666be65e88aae1f5540d3926eeaed13733
KG <kgitts98@gmail.com>
2023-01-30 07:33:05 -0800

    .

--- divisible_by_three.py
+++ divisible_by_three.py
@@ -15,7 +15,20 @@ def divisible_by_three(st):
   return int(st) in [0, 3, 6, 9]
   '''
 
+  '''
   if len(st) == 1:
     return st in '369'
   return divisible_by_three(str(sum(map(int, st))))
+  '''
+
+  s = sum([int(x) for x in st])
+
+  print(s)
+
+  for x in range(1, s):
+    if x * 3 == s:
+      return True
+  return False
+
+
 print(divisible_by_three('19254'))
