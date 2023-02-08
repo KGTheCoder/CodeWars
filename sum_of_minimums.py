df1bd585f5936bd5155cbff93f66093e6ddad08c
KG <kgitts98@gmail.com>
2023-02-08 13:46:22 -0800

    ..

--- sum_of_minimums.py
+++ sum_of_minimums.py
@@ -1,5 +1,19 @@
 def sum_of_minimums(numbers):
-  pass
+  '''
+  min_num = 0
+  for i in numbers:
+    for j in numbers:
+      print(min_num)
+      min_num += min(j)
+
+    return min_num
+  '''
+
+  total = 0
+  for nums in numbers:
+    total += min(nums)
+  return total 
 
 
 print(sum_of_minimums([ [ 7,9,8,6,2 ], [6,3,5,4,3], [5,8,7,4,5] ]))
+
